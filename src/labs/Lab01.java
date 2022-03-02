aacabc17ae532b627000e54dafccac62d10fdccc
Z4N3P <89420143+Z4N3P@users.noreply.github.com>
2022-03-02 08:55:26 -0600

    r3g45g
    g54g45g

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,5 +1,5 @@
 package labs;
-
+import java.util.Scanner;
 
 
 public class Lab01 {
@@ -17,15 +17,32 @@ public class Lab01 {
 	
 	public static void problem01() {
 		
+		//creating a new scanner
+		Scanner inKey = new Scanner(System.in);
+				
+		//asking for positive integers to user
+		System.out.print("Enter a positive integer here: ");
+		int userNum1 = inKey.nextInt();
+		System.out.print("Enter one more integer here: ");
+		int userNum2 = inKey.nextInt();
 		
+		double powResult = Math.pow(userNum1, userNum2);
 		
-	}
+		System.out.println(userNum1 + "^" + userNum2 + " = " + powResult + "/n");
+	} 
 	
 	
 	
 	
 	public static void problem02() {
 		
+		// creating a new scanner to get input from user
+		Scanner inKey2 = new Scanner (System.in);
+		
+		System.out.print("Enter a positive integer: ");
+		double userNum = inKey2.nextDouble();
+		double sqrtOfNum = Math.sqrt(userNum);
+		System.out.println("The square root of " + userNum + " is " +sqrtOfNum + "./n");
 		
 		
 	}
@@ -34,8 +51,13 @@ public class Lab01 {
 	
 
 	public static void problem03() {
-		
-		
+		Scanner inKey3 = new Scanner(System.in);
+		System.out.print("Length of side A: ");
+		double sideA = inKey3.nextDouble();
+		System.out.print("Length of side B: ");
+		double sideB = inKey3.nextDouble();
+		double hypotenuse = Math.sqrt((Math.pow(sideA, 2)) + (Math.pow(sideB, 2)));
+		System.out.println("Hypotenuse = " + hypotenuse + "\n");
 		
 
 	}
@@ -44,7 +66,26 @@ public class Lab01 {
 	
 	
 	public static void problem04() {
+		Scanner inKey4 = new Scanner(System.in);
 		
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		//variable to store number user inputs 
+		int userNum = 1;
+		boolean not0 = true;
+		while(not0) {
+			System.out.print("Enter a integer: ");
+			userNum = inKey4.nextInt();
+			if (userNum == 0) {
+				not0 = false;
+			}
+			
+			max = Math.max(max, userNum);
+			min = Math.min(min, userNum);
+		}
+		
+			System.out.print("Max = " + max);
+			System.out.println("Min = " + min);
 		
 		
 	}
--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -1,56 +1,58 @@
 package labs;
 
+import java.util.Scanner;
 
-
-public class Lab02 {
+public class PerformanceTask {
 
 	public static void main(String[] args) {
 		
-		problem01();
-		problem02();
-		problem03();
-		problem04();
 		
+		
+	Scanner inKey1 = new Scanner(System.in);	
+	
+	System.out.print("Enter launch velocity (m/s) >>> ");	
+	
+	int launchVelocity = inKey1.nextInt();
+	
+	System.out.print("Enter launch angle (degrees) >>> ");	
+	int launchAngle = inKey1.nextInt();	
+	System.out.println("Projectile's path: ");
+			
+	int time=0;
+	int yPos = 0;
+	double xPos=Math.cos(Math.toRadians(launchAngle)) * time * launchVelocity;
+	
+	
+	double ypos=Math.sin(Math.toRadians(launchAngle)) * time * launchVelocity - 0.5 * 9.8 * Math.pow(launchAngle, launchVelocity);
+	boolean launch = true;
+	System.out.println("Time: " + time);
+	System.out.println("xPos:  " + xPos +" m");
+	System.out.println( "yPos:  " + yPos + " m");
+	
+	while (launch) {
+		
+	
+	
+	     time++;
+	     xPos=Math.cos(Math.toRadians(launchAngle)) * time * launchVelocity;
+	     
+	     yPos=Math.sin(Math.toRadians(launchAngle)) * time * launchVelocity - 0.5 * 9.8 * Math.pow();
+	     
+	     if (yPos < 0) {
+	         yPos=0;
+	
+	
+	         
+	     }
+	     System.out.println("Time: " + time );
+	     
+	   System.out.println("xPos:  " + xPos +" m");
+	   
+	   System.out.println( "yPos:  " + yPos + " m");
+
+         if (yPos==0) {
+             launch=false;
+       }   
+	}    
 	}
-	
-
-	
-	public static void problem01() {
-		
-		
-		
-		
 	}
-	
-	
-	
-	
-	public static void problem02() {
-		
-		
-		
-	}
-
-	
-	
-
-	public static void problem03() {
-
-		
-		
-	}
-
-
-	
-	
-	public static void problem04() {
-		
-		
-		
-		
-	}
-	
-	
-
-	
-}
